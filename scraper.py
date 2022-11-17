1686300ef86ff7dab5e760e0e9dd5616a1b84792
Minp45 <72828584+Minp45@users.noreply.github.com>
2022-11-17 19:56:29 +0000

    Successfully send email but unable to deploy to AWS lambo

--- scraper.py
+++ scraper.py
@@ -1,13 +1,16 @@
-import pandas as pd
+# import pandas as pd
+import smtplib
+import os
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 
-
 # first download the page
 # alway keep you code nice and short 5-10 line ideal
 YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
 
 def get_driver():
   chrome_options = Options()
@@ -17,6 +20,7 @@ def get_driver():
   driver = webdriver.Chrome(options=chrome_options)
   return driver
 
+
 def get_videos(driver):
   VIDEO_DIV_TAG = 'ytd-video-renderer'
   driver.get(YOUTUBE_TRENDING_URL)
@@ -25,20 +29,21 @@ def get_videos(driver):
   videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
   return videos
 
+
 def parse_video(video):
   title_tag = video.find_element(By.ID, 'video-title')
   title = title_tag.text
   url = title_tag.get_attribute('href')
-  
+
   thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
   thumbnail_url = thumbnail_tag.get_attribute('src')
-  
+
   channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
   channel_name = channel_div.text
 
   description = video.find_element(By.ID, 'description-text').text
 
-  return{
+  return {
     'title': title,
     'url': url,
     'thumbnail': channel_name,
@@ -46,21 +51,55 @@ def parse_video(video):
   }
 
 
+def send_email(body):
+  try:
+    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server_ssl.ehlo()
+
+    SENDER_EMAIL = 'miketang667@gmail.com'
+    RECEIVER_EMAIL = 'miketang667@gmail.com'
+    SENDER_PASSWORD = os.environ['GMAIL_PASSWORD']
+
+    print('Password: ', SENDER_PASSWORD)
+
+    subject = 'YouTube Trending Videos'
+
+    email_text = f"""
+    From: {SENDER_EMAIL}
+    To: {RECEIVER_EMAIL}
+    Subject: {subject}
+
+
+    {body}
+    """
+
+    server_ssl.login(SENDER_EMAIL, SENDER_PASSWORD)
+    server_ssl.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, email_text)
+    server_ssl.close()
+  except:
+    print('Something went wrong...')
+
+
 if __name__ == "__main__":
   print('creating driver')
   driver = get_driver()
-  
+
   print('Fetching trending videos')
   videos = get_videos(driver)
 
   print(f'Found {len(videos)} videos')
-  
+
   print('parsing top 10 videos')
 
   videos_data = [parse_video(video) for video in videos[:10]]
 
   print('Save the data to a CSV')
-  videos_df = pd.DataFrame(videos_data)
-  print(videos_df)
-  videos_df.to_csv('trending.csv')
+  # videos_df = pd.DataFrame(videos_data)
+  # print(videos_df)
+  # videos_df.to_csv('trending.csv', index=None)
 
+  print("send the result over email")
+  body = json.dumps(videos_data,indent = 2)
+  send_email(body)
+
+  print("Finished")
